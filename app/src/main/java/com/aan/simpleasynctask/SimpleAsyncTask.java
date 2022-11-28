69c24d0e3792bd2c9fca2c5287829ab614585be1
astnsnaa <sucitraaansentosa@gmail.com>
2022-11-28 10:04:47 +0700

    first commit

---
+++ app/src/main/java/com/aan/simpleasynctask/SimpleAsyncTask.java
@@ -0,0 +1,12 @@
+package com.aan.simpleasynctask;
+
+import android.widget.TextView;
+
+public class i {
+    public SimpleAsyncTask(TextView mTextView) {
+    }
+
+    public void execute() {
+
+    }
+}
